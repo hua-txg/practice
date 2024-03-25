557ace13dcd489ca2c099afd24bc54f62c8d83a3
hua-txg <hua.txg@gmail.com>
2024-03-25 17:02:03 +0800

    filter

--- reviews-analytics.py
+++ reviews-analytics.py
@@ -15,3 +15,10 @@ sum_len = 0
 for d in data:
 	sum_len = sum_len + len(d)
 print('留言的平均長度為', sum_len/len(data))
+
+#篩選出每筆留言小於100個字
+new = []
+for d in data:  #d是一個留言
+	if len(d) < 100:
+		new.append(d)
+print('一共有', len(new), '筆留言長度小於100')
