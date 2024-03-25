d98542b171cd494e1919afead67e055c7ce63618
hua-txg <hua.txg@gmail.com>
2024-03-25 16:30:16 +0800

    average length

--- reviews-analytics.py
+++ reviews-analytics.py
@@ -8,3 +8,10 @@ with open('reviews.txt', 'r') as f:
 			print(len(data))
 print(len(data))
 print(data[0])
+print('檔案讀取完了，總共有', len(data), '筆資料')
+
+#計算平均留言的長度，將每一筆留言的長度sum計算出來，除以留言筆數，就是留言的平均長度
+sum_len = 0
+for d in data:
+	sum_len = sum_len + len(d)
+print('留言的平均長度為', sum_len/len(data))
