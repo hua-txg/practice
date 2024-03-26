b26dcf1d42b2b6c153e30e5047ed3b524bce88c5
hua-txg <hua.txg@gmail.com>
2024-03-26 11:21:17 +0800

    輸入商品名稱、價格 用for loop 印出輸入的商品名稱、價格

--- product.py
+++ product.py
@@ -3,5 +3,15 @@ while True:
 	name = input('請輸入商品名稱：')
 	if name == 'q':
 		break
-	products.append(name)
+	price = input('請輸入商品價格：')
+	p = []
+	p.append(name)
+	p.append(price)
+	products.append(p)
 print(products)
+
+#簡寫 products.append([name, price])
+
+for p in products:
+	print(p)
+	print(p[0], '的價格是', p[1])
