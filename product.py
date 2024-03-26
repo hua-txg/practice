7bda460ce119a00504ca353c0ee844babdcd7e34
hua-txg <hua.txg@gmail.com>
2024-03-26 15:38:37 +0800

    讀取products.csv 放在記憶體

--- product.py
+++ product.py
@@ -1,4 +1,18 @@
+#讀取檔案
 products = []
+with open('products.csv', 'r', encoding='utf-8') as f:
+	for line in f:
+		# s = line.strip().split(',')
+		# name = s[0]
+		# price = s[1]
+		name, price = line.strip().split(',')
+		products.append([name, price])
+	print(products)
+
+
+
+
+# products = []
 while True:
 	name = input('請輸入商品名稱：')
 	if name == 'q':
@@ -23,7 +37,7 @@ for p in products:
 # 		f.write(p[0]+','+str(p[1])+'\n')
 
 #寫入欄位名稱
-with open('products.csv', 'w') as f:
+with open('products.csv', 'w', encoding='utf-8') as f:
 	f.write('商品,價格\n')
 	for p in products:
 		f.write(p[0]+','+str(p[1])+'\n')
