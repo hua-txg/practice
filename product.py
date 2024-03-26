a83670402ef1520f4b53b1413e325504539c61aa
hua-txg <hua.txg@gmail.com>
2024-03-26 13:34:14 +0800

    寫入欄位名稱

--- product.py
+++ product.py
@@ -4,6 +4,7 @@ while True:
 	if name == 'q':
 		break
 	price = input('請輸入商品價格：')
+	price = int(price)
 	p = []
 	p.append(name)
 	p.append(price)
@@ -17,12 +18,14 @@ for p in products:
 	print(p[0], '的價格是', p[1])
 
 #輸入結果寫入檔案
-with open('products.txt', 'w') as f:
-	for p in products:
-		f.write(p[0]+','+p[1]+'\n')
+# with open('products.txt', 'w') as f:
+# 	for p in products:
+# 		f.write(p[0]+','+str(p[1])+'\n')
 
+#寫入欄位名稱
 with open('products.csv', 'w') as f:
+	f.write('商品,價格\n')
 	for p in products:
-		f.write(p[0]+','+p[1]+'\n')
+		f.write(p[0]+','+str(p[1])+'\n')
 
 
