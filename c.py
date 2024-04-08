1bac27815324769a3fb904621feee9f53253a07e
hua-txg <hua.txg@gmail.com>
2024-04-08 14:29:17 +0800

    初始化就執行所有function, class第一個參數self，我們放入的是第二個參數

--- c.py
+++ c.py
@@ -1,13 +1,16 @@
 class Student:
 	def __init__(self):
 		print('我誕生了')
-	def do_hw(self):
-		print('我在做作業')
+		self.do_hw('英文')
+		self.study()
+		self.sleep()
+	def do_hw(self, hw):
+		print('我在做作業', hw)
 	def study(self):
 		print('我在讀書')
+	def sleep(self):
+		print('i am sleeping')
    
 
 s = Student()
 
-s.do_hw()
-s.study()
