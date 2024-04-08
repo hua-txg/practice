957cd18e4e22ed7e6d3063b3b95dcedd6246c417
hua-txg <hua.txg@gmail.com>
2024-04-08 16:27:30 +0800

    re_color

--- c2.py
+++ c2.py
@@ -1,5 +1,12 @@
 class Desk:
-	def __init__(self):
+	def __init__(self, color):
+		self.color = color
 		print('我被制造出來了')
+	def re_color(self, new_color):
+		self.color = new_color
 
-d = Desk()  #instantiation實體化(創作出物件), 存成d
+d = Desk('Blue')  #instantiation實體化(創作出物件), 存成d
+d.re_color('red')
+print(d.color)
+
+
